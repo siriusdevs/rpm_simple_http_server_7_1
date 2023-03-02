edcc1761a92b08d0a6121066afd04bf13ffaf51b
siriusdevs <tenigin.aa@talantiuspeh.ru>
2023-03-02 15:15:09 +0300

    stable get/post

--- config.py
+++ config.py
@@ -6,3 +6,5 @@ page_7_2 = '/7_2'
 page_7_1 = '/7_1'
 PAGES = page_7_1, page_7_2
 SELECT_GROUPS = 'SELECT * FROM group_{group_num}'
+INSERT = 'INSERT INTO group_{group_num} VALUES (\'{name}\')'
+DELETE = 'DELETE FROM group_{group_num} WHERE name=\'{name}\''
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from psycopg2 import connect
 from dotenv import load_dotenv
 from os import getenv
 from config import *
+from json import loads
+
 
 load_dotenv()
 
@@ -24,16 +26,27 @@ def get_data(path: str) -> dict:
         }
 
 
+def change_db(path: str, name: str, request: str) -> bool:
+    global db_cursor, db_connection
+    try:
+        db_cursor.execute(request.format(group_num=path[1:], name=name))
+    except Exception as error:
+        print(f'change_db error: {error}')
+        return False
+    else:
+        db_connection.commit()
+        return bool(db_cursor.rowcount)
+
+
 def get_template(path: str) -> str:
     if path in PAGES:
         return GROUP_PAGE
     return MAIN_PAGE
-        
+
 
 class CustomHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        print(f'request data:\n {self.__dict__}')
         self.send_response(200)
         self.send_header('Content-type', 'html')
         self.end_headers()
@@ -43,6 +56,29 @@ class CustomHandler(BaseHTTPRequestHandler):
             if self.path in PAGES:
                 page = page.format(**get_data(self.path))
             self.wfile.write(page.encode())
+
+
+    def respond(self, http_code: int, msg: str):
+        self.send_response(http_code)
+        self.send_header('Content-type', 'text')
+        self.end_headers()
+        self.wfile.write(msg.encode())
+
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        request_data = loads(self.rfile.read(content_length).decode())
+        name = request_data.get('name')
+        change_db(self.path, name, INSERT)
+        self.respond(200, 'POST OK')
+
+
+    def do_DELETE(self):
+        content_length = int(self.headers['Content-Length'])
+        request_data = loads(self.rfile.read(content_length).decode())
+        name = request_data.get('name')
+        change_db(self.path, name, DELETE)
+        self.respond(200, 'DELETE OK')
 
 
 if __name__ == '__main__':
